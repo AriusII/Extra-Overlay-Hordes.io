9c8a7b197e95a1b1b9e1a769d687500105ac96c7
AriusII <dragonmouff@gmail.com>
2022-04-13 03:24:12 +0200

    init

--- Core.js
+++ Core.js
@@ -27,12 +27,6 @@ class Core {
                 }
             }, 1000);
 
-            document.addEventListener('keydown', e => {
-                if (e.altKey && e.key === 'L') {
-                    this.toggleUI();
-                }
-            });
-
             let world = origin.match(/([_a-zA-Z0-9]*?)\.entities\.array\.length;/)[1];
             let coder = origin.match(/\,([_a-zA-Z0-9]*?)=\{clientPlayerInput:\{/)[1];
             let ws = origin.match(/\(([_a-zA-Z0-9]*?)=new WebSocket/)[1];
